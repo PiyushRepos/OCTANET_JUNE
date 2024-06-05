2c29300dc0f62c1f044c188991e141e65ccbeefd
Piyush kumar <piyush2004.dev@gmail.com>
2024-06-05 16:59:01 +0530

    first commit

---
+++ Landing-page/README.md
@@ -0,0 +1,2 @@
+# landing-page
+# OCTANET_JUNE
---
+++ Landing-page/src/main.js
@@ -0,0 +1,11 @@
+import '../styles/style.css';
+import '../styles/modern-normalize.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/feature.css';
+import '../styles/components/callto-action.css';
+import '../styles/components/guarantee.css';
+import '../styles/components/career.css';
+import '../styles/components/footer.css';
+import '../styles/utils.css';
+
